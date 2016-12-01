07158bfbd311419447930369419c40169093111f
Belmin <belminf@amazon.com>
2016-12-01 18:59:50 -0500

    combines both boto scripts, closes #5

--- setup_vmie_lambda.py
+++ setup_vmie_lambda.py
@@ -2,11 +2,14 @@
 
 from os.path import basename, splitext
 from zipfile import ZipFile
+from jinja2 import Template
 import argparse
 import boto3
 
-parser = argparse.ArgumentParser(description='Upload lambda code to S3')
+parser = argparse.ArgumentParser(description='Create an S3 bucket for automatic import of OVAs')
+parser.add_argument('stackname', metavar='STACK_NAME', help='CloudFormation stack name')
 parser.add_argument('bucket', metavar='S3_BUCKET', help='S3 bucket name for Lambda code')
+parser.add_argument('email', metavar='EMAIL', help='e-mail address for notificaitons')
 args = parser.parse_args()
 
 LAMBDA_DEPLOYMENT_FILES = (
@@ -35,5 +38,28 @@ for filename in LAMBDA_DEPLOYMENT_FILES:
     s3.Bucket(S3_BUCKET_NAME).put_object(Key=zip_fn, Body=open(zip_fn))
 
     print('Uploaded {} to {}...'.format(zip_fn, S3_BUCKET_NAME))
+
+
+# Load template
+template = Template(open('cfn-template.yaml.j2').read())
+
+# Render template
+cfn_template = template.render(
+    email=args.email,
+    bucket=args.bucket
+)
+
+# CloudFormation client
+client = boto3.client('cloudformation')
+
+# Create CloudFormation stack
+response = client.create_stack(
+    StackName=args.stackname,
+    TemplateBody=cfn_template,
+    Capabilities=('CAPABILITY_NAMED_IAM', 'CAPABILITY_IAM'),
+)
+
+# Print response
+print(response)
 
 print('Done.')
