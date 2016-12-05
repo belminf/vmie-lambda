b37db79e9472a48ac497f161fe682b89c2d27394
Belmin <belminf@gmail.com>
2016-12-04 19:53:17 -0500

    refacted lambda code for simplicity

--- lambda_code.py
+++ lambda_code.py
@@ -4,8 +4,6 @@ import os
 
 # Handles new OVA added to S3 bucket
 def handle_new_images(event, context):
-
-    # Grabbing S3 Bucket and S3 Object Key from S3 Event Object
     s3_key = event[u'Records'][0][u's3'][u'object'][u'key']
     s3_bucket = event[u'Records'][0][u's3'][u'bucket'][u'name']
 
@@ -34,33 +32,23 @@ def handle_new_images(event, context):
 
 # Handles a scan of tasks
 def handle_task_scans(event, context):
-
-    ddb = boto3.resource('dynamodb')
-    ec2 = boto3.client('ec2')
-    status_table = ddb.Table(os.environ['DYNAMO_TABLE'])
-
-    scan_items = status_table.scan(ConsistentRead=True)['Items']
-    task_ids = [i['task_id'] for i in scan_items]
-    task_vms = {i['task_id']: i['vm_name'] for i in scan_items}
-
-    # Get current status of tasks
-    current_status = ec2.describe_import_image_tasks(ImportTaskIds=task_ids)
+    task_ids = get_recorded_task_ids()
+    task_descriptions = get_task_descriptions(task_ids)
 
     # Process task statuses
-    for t in current_status['ImportImageTasks']:
+    for task_id in task_ids:
+        description = task_descriptions.get(task_id)
 
-        task_id = t['ImportTaskId']
-        task_status = t['Status']
-        vm_name = task_vms[task_id]
+        # Log error if we cannot find the description
+        if not description:
+            print('Error: Could not find description for task {}'.format(task_id))
+            continue
 
         # If status not done, skip
-        if task_status not in ('completed', 'deleted'):
+        if description['status'] not in ('completed', 'deleted'):
             continue
 
         # ASSERT: task is done
-
-        # Delete item now
-        status_table.delete_item(Key={'task_id': task_id})
 
         # Send notification
         send_notification(
@@ -70,34 +58,31 @@ def handle_task_scans(event, context):
                 'VM import task is complete.',
                 '',
                 'Task ID: {}'.format(task_id),
-                'Image: {}'.format(vm_name),
-                'Status: {}'.format(task_status),
-                '',
-                'You will be notified when the task is completed.'
+                'Image: {}'.format(description['image']),
+                'Status: {} - {}'.format(description['status'], description['msg']),
             ))
         )
+
+        # Delete item now
+        delete_recorded_task(task_id)
 
     # Toggle checker based on number of tasks
     toggle_checker(len(task_ids) > 0)
 
 
 def start_image_import(s3_bucket, s3_key):
-
     ec2 = boto3.client('ec2')
 
     return ec2.import_image(
         Description='Lambda_VMIE',
-        DiskContainers=[
-            {
-                'Description': 'Import_OVA',
-                'Format': 'ova',
-                'UserBucket':
-                {
-                    'S3Bucket': s3_bucket,
-                    'S3Key': s3_key
-                },
+        DiskContainers=[{
+            'Description': 'Import_OVA',
+            'Format': 'ova',
+            'UserBucket': {
+                'S3Bucket': s3_bucket,
+                'S3Key': s3_key
             },
-        ],
+        }],
         RoleName=os.environ['VMIE_IAM_ROLE'],
         LicenseType='BYOL'
     )
@@ -112,7 +97,6 @@ def send_notification(topic, subject, msg):
 
 
 def toggle_checker(toggle=True):
-
     events = boto3.client('events')
     rule_name = os.environ['SCHEDULE_RULE']
 
@@ -121,9 +105,10 @@ def toggle_checker(toggle=True):
     else:
         events.disable_rule(Name=rule_name)
 
+    print('Schedule rule toggled to {}'.format('ON' if toggle else 'OFF'))
+
 
 def record_task(task_id, vm_name):
-
     ddb = boto3.client('dynamodb')
 
     ddb.put_item(
@@ -133,3 +118,41 @@ def record_task(task_id, vm_name):
             'vm_name': {'S': vm_name},
         }
     )
+
+    print('Recorded task in DynamoDB: {} ({})'.format(task_id, vm_name))
+
+
+def get_recorded_task_ids():
+    ddb = boto3.resource('dynamodb')
+    status_table = ddb.Table(os.environ['DYNAMO_TABLE'])
+
+    scan_items = status_table.scan(ConsistentRead=True)['Items']
+
+    return [i['task_id'] for i in scan_items]
+
+
+def delete_recorded_task(task_id):
+    ddb = boto3.resource('dynamodb')
+    status_table = ddb.Table(os.environ['DYNAMO_TABLE'])
+
+    status_table.delete_item(Key={'task_id': task_id})
+
+    print('Deleted task in DynamoDB: {}'.format(task_id))
+
+
+def get_task_descriptions(task_ids):
+    ec2 = boto3.client('ec2')
+
+    descriptions = ec2.describe_import_image_tasks(ImportTaskIds=task_ids)
+
+    task_descriptions = {}
+
+    for task in descriptions:
+        task_descriptions[task['ImportTaskId']] = {
+            'status': task['Status'],
+            'msg': task['StatusMessage'],
+            'image': task['UserBucket'].get('S3Key'),
+            'snapshot': task['SnapshotId'].get('SnapshotId'),
+        }
+
+    return task_descriptions
